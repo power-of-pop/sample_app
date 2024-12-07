62404608fd2cb417819dfed7666b58e762204196
EC2 Default User <ec2-user@ip-172-31-39-216.ap-northeast-1.compute.internal>
2024-12-07 15:18:44 +0000

    [Update] 3-12

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,9 +6,10 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-      redirect_to list_path(list.id)
+      flash[:notice] = "投稿に成功しました。"
+      redirect_to list_path(@list.id)
     else
-      render :index
+      render :new
     end
   end
 
